2e1e5317a9d58fee7ff243f9eeb96c1f55d2debf
carrottechplus <blohzzang@gmail.com>
2023-06-01 12:55:57 +0900

    시간 불러오기 script 고도화

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 const numbers = document.querySelectorAll('.screen span');
 
 setInterval(() => {
+	const times = getTime();
+	times.forEach((num, idx) => setTime(num, idx));
+}, 1000);
+
+function getTime() {
 	const now = new Date();
 	let hr = now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
 
-	if (hr < 10) hr = '0' + hr;
-	if (min < 10) min = '0' + min;
-	if (sec < 10) sec = '0' + sec;
+	return [hr, min, sec];
+}
 
-	numbers[0].innerText = hr;
-	numbers[1].innerText = min;
-	numbers[2].innerText = sec;
-}, 1000);
+function setTime(num, index) {
+	if (num < 10) num = '0' + num;
+	else num = num;
+
+	numbers[index].innerText = num;
+}
