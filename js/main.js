649604d7c5b38b640fb45a13fd40847426e73ff3
carrottechplus <blohzzang@gmail.com>
2023-06-02 10:52:31 +0900

    시간대별 class 구분 script 고도화 및 dark class 추가

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 const main = document.querySelector('main');
 const numbers = main.querySelectorAll('.screen span');
 const [am, pm] = main.querySelectorAll('.screen em');
-// console.log([am, pm]);
 
 setInterval(() => {
 	changeTheme();
 	const times = getTime();
 	times.forEach((num, idx) => setTime(num, idx));
 }, 1000);
+
+function changeTheme() {
+	const hr = new Date().getHours();
+	const data = [
+		{ cond: hr >= 5 && hr < 11, name: 'morning' },
+		{ cond: hr >= 11 && hr < 16, name: 'afternoon' },
+		{ cond: hr >= 16 && hr < 19, name: 'evening' },
+		{ cond: hr >= 19 || hr < 5, name: 'night' },
+	];
+
+	data.forEach((item) => {
+		if (item.cond) {
+			main.className = '';
+			main.classList.add(item.name);
+		}
+	});
+
+	// if (main.classList.contains('afternoon')) main.classList.add('dark');
+	// else main.classList.remove('dark');
+
+	main.classList.contains('afternoon') ? main.classList.add('dark') : main.classList.remove('dark');
+}
 
 function getTime() {
 	// 시간을 가져오는 함수
@@ -32,29 +53,4 @@ function setTime(num, index) {
 	else num = num;
 
 	numbers[index].innerText = num;
-}
-
-function changeTheme() {
-	const hr = new Date().getHours();
-	// console.log(hr);
-
-	if (hr >= 5 && hr < 11) {
-		main.className = ''; //초기화
-		main.classList.add('morning');
-	}
-
-	if (hr >= 11 && hr < 16) {
-		main.className = ''; //초기화
-		main.classList.add('afternoon');
-	}
-
-	if (hr >= 16 && hr < 19) {
-		main.className = ''; //초기화
-		main.classList.add('evening');
-	}
-
-	if (hr >= 19 || hr < 5) {
-		main.className = ''; //초기화
-		main.classList.add('night');
-	}
 }
